de1881d98c0017afc726766e5374414bd1c608be
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-12-25 18:36:07 +0700

    first commit

---
+++ src/main/java/com/example/DATN_Fashion_Shop_BE/DatnFashionShopBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.DATN_Fashion_Shop_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnFashionShopBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnFashionShopBeApplication.class, args);
+	}
+
+}
