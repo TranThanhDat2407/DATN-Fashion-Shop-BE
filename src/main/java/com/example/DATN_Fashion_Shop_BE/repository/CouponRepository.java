aad32c17786079ee07280fe355427b1d2b8eca85
ducminhhh <doducminh141000@gmail.com>
2025-02-27 12:39:55 +0700

    update code coupon

--- src/main/java/com/example/DATN_Fashion_Shop_BE/repository/CouponRepository.java
+++ src/main/java/com/example/DATN_Fashion_Shop_BE/repository/CouponRepository.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 @Repository
 public interface CouponRepository extends JpaRepository<Coupon,Long>,JpaSpecificationExecutor<Coupon> {
-    Optional<Coupon> findByCode(String couponCode);
+    Optional<Coupon> findFirstByCode(String code);
 
     List<Coupon> findByIsGlobalTrueAndIsActiveTrue();
 
--- src/main/java/com/example/DATN_Fashion_Shop_BE/service/CouponService.java
+++ src/main/java/com/example/DATN_Fashion_Shop_BE/service/CouponService.java
@@ -35,11 +35,10 @@ public class CouponService {
 
 
     public boolean applyCoupon(Long userId, String couponCode) {
-        Optional<Coupon> couponOpt = couponRepository.findByCode(couponCode);
+        Optional<Coupon> couponOpt = couponRepository.findFirstByCode(couponCode);
         if (couponOpt.isEmpty()) {
             throw new RuntimeException("Mã giảm giá không tồn tại.");
         }
-
         Coupon coupon = couponOpt.get();
         if (!coupon.getIsActive()) {
             throw new RuntimeException("Mã giảm giá không còn hiệu lực.");
@@ -58,7 +57,6 @@ public class CouponService {
                 .used(true)
                 .build();
         userCouponUsageRepository.save(usage);
-
         return true;
     }
     @Transactional
